fb2169f1ce7022ed60cb76b47f2e186cf9e07f7f
Quang <co@99.co>
2020-05-15 18:21:38 +0800

    update(endpoints): listing api

--- src/actions/listings.js
+++ src/actions/listings.js
@@ -17,7 +17,7 @@ export function fetchListingsError() {
 }
 
 export function fetchData(dispatch) {
-  superagent.get('http://co.99.co/api/v1/web/search/listings?query_limit=radius&query_type=city&page_size=20&zoom=11&listing_type=sale&query_coords=1.3039947,103.8298507&page_num=1&radius_max=1000&map_bounds=1.5827095153768858,103.49449749970108,1.1090706240313446,104.12483807587296')
+  superagent.get('https://www.99.co/api/v1/web/search/listings?query_limit=radius&query_type=city&page_size=20&zoom=11&listing_type=sale&query_coords=1.3039947,103.8298507&page_num=1&radius_max=1000&map_bounds=1.5827095153768858,103.49449749970108,1.1090706240313446,104.12483807587296')
             .then((res) => {
               const data = res.body.data;
               const listings = data.sections[0].listings;
