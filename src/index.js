88a8b29bbfc2f8f46b2a715b1af78e3b01e8d860
AndrewThian <andrewbobbythian@gmail.com>
2020-05-20 14:58:37 +0800

    cleanup(bump-semver): remove service worker implementation from create-react-app

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import log from './middlewares/log';
-import App from './App';
-import rootReducer from './reducers';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import log from "./middlewares/log";
+import App from "./App";
+import rootReducer from "./reducers";
+import "./index.css";
 
 const store = createStore(rootReducer, applyMiddleware(log, thunk));
-  
+
 function render() {
   ReactDOM.render(
     <Provider store={store}>
       <App />
     </Provider>,
-    document.getElementById('root'));
+    document.getElementById("root")
+  );
 }
 
 render();
-registerServiceWorker();
